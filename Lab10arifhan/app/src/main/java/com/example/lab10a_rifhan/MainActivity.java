8ca1a638ee2342d6f1abf5c959453096885d2968
rifhan <119849760+rifhanbusy@users.noreply.github.com>
2026-06-22 16:51:56 +0800

    Add files via upload

---
+++ Lab10arifhan/app/src/main/java/com/example/lab10a_rifhan/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.lab10a_rifhan;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView web;
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        web = findViewById(R.id.webView);
+// Enable JavaScript
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+// Ensure links open within the WebView instead of an external browser
+        web.setWebViewClient(new WebViewClient());
+// Load UiTM YouTube Channel
+        web.loadUrl("https://www.youtube.com/UiTMChannel");
+    }
+}
